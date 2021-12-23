6302fb75b64265f9e4c552e6781adf8623879566
Edo FR <edo.f12345@gmail.com>
2021-12-23 14:52:28 +0700

    LOGIN

--- AplikasiMagangklmpk3/AplikasiMagangklmpk3/Form1.vb
+++ AplikasiMagangklmpk3/AplikasiMagangklmpk3/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FormBorderStyle = Windows.Forms.FormBorderStyle.FixedDialog
+        BackColor = Color.White
+        StartPosition = FormStartPosition.CenterScreen
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Close()
+
+    End Sub
 End Class
