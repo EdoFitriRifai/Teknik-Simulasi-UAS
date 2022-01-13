8695395cb755ade6a770b13f32882502c0ee33ed
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-01-13 22:56:22 +0700

    Formulir fix

--- AppMagang3/AppMagang3/HalamanFormulirMagang.Designer.vb
+++ AppMagang3/AppMagang3/HalamanFormulirMagang.Designer.vb
@@ -23,47 +23,39 @@ Partial Class HalamanFormulirMagang
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
+        Me.cmbDurasiMagang = New System.Windows.Forms.ComboBox()
+        Me.btnKirim = New System.Windows.Forms.Button()
+        Me.txtAlamatInstansi = New System.Windows.Forms.TextBox()
+        Me.txtNamaInstansi = New System.Windows.Forms.TextBox()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.txtFakultas = New System.Windows.Forms.TextBox()
+        Me.txtSemester = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNpm = New System.Windows.Forms.TextBox()
         Me.Label11 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
-        Me.TextBox7 = New System.Windows.Forms.TextBox()
-        Me.TextBox10 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel2
         '
-        Me.Panel2.Controls.Add(Me.ComboBox2)
-        Me.Panel2.Controls.Add(Me.ComboBox1)
-        Me.Panel2.Controls.Add(Me.Button1)
-        Me.Panel2.Controls.Add(Me.TextBox10)
-        Me.Panel2.Controls.Add(Me.TextBox7)
-        Me.Panel2.Controls.Add(Me.TextBox6)
-        Me.Panel2.Controls.Add(Me.TextBox5)
-        Me.Panel2.Controls.Add(Me.TextBox4)
-        Me.Panel2.Controls.Add(Me.TextBox3)
-        Me.Panel2.Controls.Add(Me.TextBox2)
-        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.cmbDurasiMagang)
+        Me.Panel2.Controls.Add(Me.btnKirim)
+        Me.Panel2.Controls.Add(Me.txtAlamatInstansi)
+        Me.Panel2.Controls.Add(Me.txtNamaInstansi)
+        Me.Panel2.Controls.Add(Me.txtProdi)
+        Me.Panel2.Controls.Add(Me.txtFakultas)
+        Me.Panel2.Controls.Add(Me.txtSemester)
+        Me.Panel2.Controls.Add(Me.txtNama)
+        Me.Panel2.Controls.Add(Me.txtNpm)
         Me.Panel2.Controls.Add(Me.Label11)
-        Me.Panel2.Controls.Add(Me.Label10)
-        Me.Panel2.Controls.Add(Me.Label9)
         Me.Panel2.Controls.Add(Me.Label8)
         Me.Panel2.Controls.Add(Me.Label7)
         Me.Panel2.Controls.Add(Me.Label6)
@@ -78,75 +70,83 @@ Partial Class HalamanFormulirMagang
         Me.Panel2.Size = New System.Drawing.Size(784, 484)
         Me.Panel2.TabIndex = 1
         '
-        'Label1
+        'cmbDurasiMagang
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Candara", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(192, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(380, 49)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "FORMULIR MAGANG"
+        Me.cmbDurasiMagang.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDurasiMagang.FormattingEnabled = True
+        Me.cmbDurasiMagang.Items.AddRange(New Object() {"30"})
+        Me.cmbDurasiMagang.Location = New System.Drawing.Point(237, 307)
+        Me.cmbDurasiMagang.Name = "cmbDurasiMagang"
+        Me.cmbDurasiMagang.Size = New System.Drawing.Size(301, 21)
+        Me.cmbDurasiMagang.TabIndex = 23
         '
-        'Label2
+        'btnKirim
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(59, 128)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(37, 17)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "NPM"
+        Me.btnKirim.Location = New System.Drawing.Point(237, 334)
+        Me.btnKirim.Name = "btnKirim"
+        Me.btnKirim.Size = New System.Drawing.Size(79, 30)
+        Me.btnKirim.TabIndex = 21
+        Me.btnKirim.Text = "Kirim"
+        Me.btnKirim.UseVisualStyleBackColor = True
         '
-        'Label3
+        'txtAlamatInstansi
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(59, 153)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(43, 17)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Nama"
+        Me.txtAlamatInstansi.Location = New System.Drawing.Point(237, 281)
+        Me.txtAlamatInstansi.Name = "txtAlamatInstansi"
+        Me.txtAlamatInstansi.Size = New System.Drawing.Size(301, 20)
+        Me.txtAlamatInstansi.TabIndex = 17
         '
-        'Label4
+        'txtNamaInstansi
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(59, 179)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(64, 17)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Semester"
+        Me.txtNamaInstansi.Location = New System.Drawing.Point(237, 255)
+        Me.txtNamaInstansi.Name = "txtNamaInstansi"
+        Me.txtNamaInstansi.Size = New System.Drawing.Size(301, 20)
+        Me.txtNamaInstansi.TabIndex = 16
         '
-        'Label5
+        'txtProdi
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(59, 205)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(59, 17)
-        Me.Label5.TabIndex = 4
-        Me.Label5.Text = "Fakultas"
+        Me.txtProdi.Location = New System.Drawing.Point(237, 229)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(301, 20)
+        Me.txtProdi.TabIndex = 15
         '
-        'Label6
+        'txtFakultas
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(59, 231)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(95, 17)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "Program Studi"
+        Me.txtFakultas.Location = New System.Drawing.Point(237, 203)
+        Me.txtFakultas.Name = "txtFakultas"
+        Me.txtFakultas.Size = New System.Drawing.Size(301, 20)
+        Me.txtFakultas.TabIndex = 14
         '
-        'Label7
+        'txtSemester
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(59, 257)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(144, 17)
-        Me.Label7.TabIndex = 6
-        Me.Label7.Text = "Nama Tempat Magang"
+        Me.txtSemester.Location = New System.Drawing.Point(237, 177)
+        Me.txtSemester.Name = "txtSemester"
+        Me.txtSemester.Size = New System.Drawing.Size(301, 20)
+        Me.txtSemester.TabIndex = 13
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(237, 151)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(301, 20)
+        Me.txtNama.TabIndex = 12
+        '
+        'txtNpm
+        '
+        Me.txtNpm.Location = New System.Drawing.Point(237, 126)
+        Me.txtNpm.Name = "txtNpm"
+        Me.txtNpm.Size = New System.Drawing.Size(301, 20)
+        Me.txtNpm.TabIndex = 11
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(59, 306)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(99, 17)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "Durasi Magang"
         '
         'Label8
         '
@@ -158,116 +158,75 @@ Partial Class HalamanFormulirMagang
         Me.Label8.TabIndex = 7
         Me.Label8.Text = "Alamat Magang"
         '
-        'Label9
+        'Label7
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(59, 309)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(150, 17)
-        Me.Label9.TabIndex = 8
-        Me.Label9.Text = "Tanggal Daftar Magang"
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(59, 257)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(144, 17)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Nama Tempat Magang"
         '
-        'Label10
+        'Label6
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label10.Location = New System.Drawing.Point(581, 126)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(73, 17)
-        Me.Label10.TabIndex = 9
-        Me.Label10.Text = "ID DAFTAR"
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(59, 231)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Program Studi"
         '
-        'Label11
+        'Label5
         '
-        Me.Label11.AutoSize = True
-        Me.Label11.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label11.Location = New System.Drawing.Point(59, 335)
-        Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(99, 17)
-        Me.Label11.TabIndex = 10
-        Me.Label11.Text = "Durasi Magang"
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(59, 205)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Fakultas"
         '
-        'TextBox1
+        'Label4
         '
-        Me.TextBox1.Location = New System.Drawing.Point(237, 126)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox1.TabIndex = 11
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(59, 179)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Semester"
         '
-        'TextBox2
+        'Label3
         '
-        Me.TextBox2.Location = New System.Drawing.Point(237, 151)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox2.TabIndex = 12
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(59, 153)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama"
         '
-        'TextBox3
+        'Label2
         '
-        Me.TextBox3.Location = New System.Drawing.Point(237, 177)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox3.TabIndex = 13
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Candara", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(59, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NPM"
         '
-        'TextBox4
+        'Label1
         '
-        Me.TextBox4.Location = New System.Drawing.Point(237, 203)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox4.TabIndex = 14
-        '
-        'TextBox5
-        '
-        Me.TextBox5.Location = New System.Drawing.Point(237, 229)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox5.TabIndex = 15
-        '
-        'TextBox6
-        '
-        Me.TextBox6.Location = New System.Drawing.Point(237, 255)
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox6.TabIndex = 16
-        '
-        'TextBox7
-        '
-        Me.TextBox7.Location = New System.Drawing.Point(237, 281)
-        Me.TextBox7.Name = "TextBox7"
-        Me.TextBox7.Size = New System.Drawing.Size(301, 20)
-        Me.TextBox7.TabIndex = 17
-        '
-        'TextBox10
-        '
-        Me.TextBox10.Location = New System.Drawing.Point(660, 125)
-        Me.TextBox10.Name = "TextBox10"
-        Me.TextBox10.Size = New System.Drawing.Size(103, 20)
-        Me.TextBox10.TabIndex = 20
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(237, 385)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(79, 30)
-        Me.Button1.TabIndex = 21
-        Me.Button1.Text = "Kirim"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(237, 309)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(301, 21)
-        Me.ComboBox1.TabIndex = 22
-        '
-        'ComboBox2
-        '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(237, 336)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(301, 21)
-        Me.ComboBox2.TabIndex = 23
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Candara", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(192, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(380, 49)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FORMULIR MAGANG"
         '
         'HalamanFormulirMagang
         '
@@ -292,20 +251,16 @@ Partial Class HalamanFormulirMagang
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents Label10 As System.Windows.Forms.Label
     Friend WithEvents Label11 As System.Windows.Forms.Label
-    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents txtAlamatInstansi As System.Windows.Forms.TextBox
+    Friend WithEvents txtNamaInstansi As System.Windows.Forms.TextBox
+    Friend WithEvents txtProdi As System.Windows.Forms.TextBox
+    Friend WithEvents txtFakultas As System.Windows.Forms.TextBox
+    Friend WithEvents txtSemester As System.Windows.Forms.TextBox
+    Friend WithEvents txtNama As System.Windows.Forms.TextBox
+    Friend WithEvents txtNpm As System.Windows.Forms.TextBox
+    Friend WithEvents btnKirim As System.Windows.Forms.Button
+    Friend WithEvents cmbDurasiMagang As System.Windows.Forms.ComboBox
 End Class
--- AppMagang3/AppMagang3/HalamanFormulirMagang.vb
+++ AppMagang3/AppMagang3/HalamanFormulirMagang.vb
@@ -1,11 +1,29 @@
 ﻿Imports System.Data.Odbc
 Public Class HalamanFormulirMagang
+    Dim tglMySQL As String
 
     Private Sub HalamanFormulirMagang_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        'FormBorderStyle = Windows.Forms.FormBorderStyle.None
     End Sub
 
     Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
 
+    End Sub
+
+    Private Sub btnKirim_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnKirim.Click
+        If txtNpm.Text = "" Or txtNama.Text = "" Or txtSemester.Text = "" Or txtFakultas.Text = "" Or txtProdi.Text = "" Or txtNamaInstansi.Text = "" Or txtAlamatInstansi.Text = "" Or cmbDurasiMagang.SelectedIndex = -1 Then
+            MsgBox("Tidak Boleh Kosong")
+            'bersih()
+        Else
+            MsgBox("Ok")
+            koneksi()
+            tglMySQL = Format(Today, "yyyy-MM-dd")
+            Dim simpan As String = "insert into tbl_formulir values ('" & "" & "','" & tglMySQL & "','" & txtNamaInstansi.Text & "','" & cmbDurasiMagang.Text & "','" & txtAlamatInstansi.Text & "','" & txtNpm.Text & "')"
+            cmd = New OdbcCommand(simpan, conn)
+            cmd.ExecuteNonQuery()
+            MsgBox("Input data berhasil")
+            'Call liatdata()
+            'Call bersih()
+        End If
     End Sub
 End Class
